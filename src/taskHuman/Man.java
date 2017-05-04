baec9d4775fa1c632387d6783cfa1d7f355c0dbe
liberstina <liberstina@gmail.com>
2017-05-04 23:56:29 +0300

    Update Man.java

--- src/taskHuman/Man.java
+++ src/taskHuman/Man.java
@@ -6,77 +6,86 @@ package taskHuman;
 public class Man {
     private int age;
     private String name;
-    private int height;
+    private static int handsQty;
+    private static int legsQty;
     private String militaryRank;
     private int screwdriversAmount;
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
-    public void setAge(int age){
+
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public int getHeight(){
-        return height;
-    }
-    public void setHeight(int height){
-        this.height = height;
+    public static int getHandsQty() {
+        return handsQty;
     }
 
-    public String getMilitaryRank(){
+    public static void setHandsQty(int handsQty) {
+        Man.handsQty = handsQty;
+    }
+
+    public static int getLegsQty() {
+        return legsQty;
+    }
+
+    public static void setLegsQty(int legsQty) {
+        Man.legsQty = legsQty;
+    }
+
+    public String getMilitaryRank() {
         return militaryRank;
     }
-    public void setMilitaryRank(String militaryRank){
+
+    public void setMilitaryRank(String militaryRank) {
         this.militaryRank = militaryRank;
     }
 
-    public int getScrewdriversAmount(){
+    public int getScrewdriversAmount() {
         return screwdriversAmount;
     }
-    public void setScrewdriversAmount(int screwdriversAmount){
+
+    public void setScrewdriversAmount(int screwdriversAmount) {
         this.screwdriversAmount = screwdriversAmount;
     }
 
-    public void eat(){
-
+    public static void eat() {
+        System.out.println("I am eating. Yammy-yammy!");
     }
 
-    public void sleep(){
-
+    public static void sleep() {
+        System.out.println("I am sleeping. Zzzzzzz");
     }
 
-    public void hammerShelf(){
-
+    public void hammerShelf() {
+        System.out.println("Knock Knock. I am the master of the house");
     }
 
-    public void becomeSoldier(){
-
+    public void becomeSoldier() {
+        System.out.println("I'm in the army now");
     }
 
-    public Man (String name, int height, String militaryRank){
-        this.name = name;
-        this.height = height;
-        this.militaryRank = militaryRank;
-        eat();
-        sleep();
-        becomeSoldier();
-    }
-
-    public Man (String name, int age, int screwdriversAmount){
-        this.name = name;
+    public Man(int age, String name, String militaryRank) {
         this.age = age;
+        this.name = name;
+        this.militaryRank = militaryRank;
+    }
+
+    public Man(int age, String name, String militaryRank, int screwdriversAmount) {
+        this.age = age;
+        this.name = name;
+        this.militaryRank = militaryRank;
         this.screwdriversAmount = screwdriversAmount;
-        eat();
-        sleep();
-        hammerShelf();
     }
 
 
